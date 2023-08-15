fe7bffcf787a188a651500fd939764ef33078c23
MateusViana13 <mviana1305@outlook.com>
2023-08-14 21:25:22 -0300

    FT: MiddleWare de exceção
    Adicionado middleware para tratar exceções da aplicação

---
+++ API_ENTITY_FRAMEWORK/MiddleWare/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,27 @@
+﻿
+public class ExceptionHandlerMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionHandlerMiddleware> _logger;
+
+    public ExceptionHandlerMiddleware(RequestDelegate next, ILogger<ExceptionHandlerMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Ocorreu uma exceção não tratada");
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            context.Response.ContentType = "text/plain";
+            await context.Response.WriteAsync("Ocorreu um erro interno. Por favor, tente novamente mais tarde.");
+        }
+    }
+}
--- API_ENTITY_FRAMEWORK/Program.cs
+++ API_ENTITY_FRAMEWORK/Program.cs
@@ -1,4 +1,6 @@
 global using API_ENTITY_FRAMEWORK.Repository;
+using API_ENTITY_FRAMEWORK.Context;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,12 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped(typeof(IBaseRepository<,>), typeof(BaseRepository<,>));
+
+builder.Services.AddDbContext<BancoDestinosContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DestinoConnection"));
+});
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -17,7 +25,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseMiddleware<ExceptionHandlerMiddleware>();
 app.UseAuthorization();
 
 app.MapControllers();
